448811b916f767de3271850522778888cd435db7
Rohit <rohitkamble10792@gmail.com>
2022-05-23 21:44:32 +0530

    Added:Welcome to Employee wages progarm

---
+++ src/com/bridgelabz/empwages/EmployeeWages.java
@@ -0,0 +1,9 @@
+package com.bridgelabz.empwages;
+
+public class EmployeeWages {
+
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to Employee Wages Program");
+    }
+}
