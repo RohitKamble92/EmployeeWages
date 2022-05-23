756fc04c6db104930812e2e4640ca771876d7fd1
Rohit <rohitkamble10792@gmail.com>
2022-05-23 22:41:46 +0530

    Added:Calculated Daily Employee Wages

--- src/com/bridgelabz/empwages/EmployeeWages.java
+++ src/com/bridgelabz/empwages/EmployeeWages.java
@@ -4,13 +4,17 @@ public class EmployeeWages {
 
     public static void main(String[] args) {
         int FULL_TIME = 1;
+        int EMP_RATE_PER_HR = 20;
+        int FULL_DAY_HR = 8;
         int empCheck = (int) Math.floor(Math.random() * 10 % 2);
-        System.out.println(empCheck);
 
+        int empHrs = 0;
         if (empCheck == FULL_TIME) {
-            System.out.println("Employee is Present");
+            empHrs = FULL_DAY_HR;
         } else {
-            System.out.println("Employee is Absent");
+           empHrs=0;
         }
+        int empWage =empHrs * EMP_RATE_PER_HR ;
+        System.out.println("Emp Wage is "+empWage);
     }
 }
