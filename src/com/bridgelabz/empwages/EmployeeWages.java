0f1cec80a84212e789c5787a5c2aba1805825ef6
Rohit <rohitkamble10792@gmail.com>
2022-05-23 23:01:44 +0530

    Added:Functionality to calculate part time employee wage

--- src/com/bridgelabz/empwages/EmployeeWages.java
+++ src/com/bridgelabz/empwages/EmployeeWages.java
@@ -4,17 +4,22 @@ public class EmployeeWages {
 
     public static void main(String[] args) {
         int FULL_TIME = 1;
+        int PART_TIME = 2;
         int EMP_RATE_PER_HR = 20;
-        int FULL_DAY_HR = 8;
-        int empCheck = (int) Math.floor(Math.random() * 10 % 2);
+        int FULL_TIME_HR = 8;
+        int PART_TIME_HR = 4;
+        int empCheck = (int) Math.floor(Math.random() * 10 % 3);
 
         int empHrs = 0;
         if (empCheck == FULL_TIME) {
-            empHrs = FULL_DAY_HR;
-        } else {
-           empHrs=0;
+            empHrs = FULL_TIME_HR;
+        } else if(empCheck==PART_TIME){
+            empHrs=PART_TIME_HR;
         }
-        int empWage =empHrs * EMP_RATE_PER_HR ;
-        System.out.println("Emp Wage is "+empWage);
+        else {
+            empHrs = 0;
+        }
+        int empWage = empHrs * EMP_RATE_PER_HR;
+        System.out.println("Emp Wage is " + empWage);
     }
 }
